e2d61bd29366ffe40c8b82a04bf9d3ff62b3185a
程云博 <chengyunbo@gyyx.cn>
2025-03-06 23:31:02 +0800

    feat: :tada:更新SQL文件

--- src/main/resources/sql/whale.sql
+++ src/main/resources/sql/whale.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80016
  File Encoding         : 65001
 
- Date: 02/03/2025 18:35:50
+ Date: 06/03/2025 23:28:06
 */
 
 SET NAMES utf8mb4;
@@ -24,18 +24,18 @@ DROP TABLE IF EXISTS `app_menu`;
 CREATE TABLE `app_menu`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '系统菜单ID',
   `menu_name` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '菜单名',
-  `menu_rank` int(11) NULL DEFAULT NULL COMMENT '菜单排名',
   `menu_flag` tinyint(1) NULL DEFAULT 1 COMMENT '菜单状态(0: 删除,1: 正常)',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `app_menu_id_uindex`(`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '系统菜单' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '系统菜单' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of app_menu
 -- ----------------------------
-INSERT INTO `app_menu` VALUES (1, '测试路由', 1, 1, '2025-03-02 18:17:57', '2025-03-02 18:18:00');
+INSERT INTO `app_menu` VALUES (1, '记账', 1, '2025-03-05 10:07:19', '2025-03-05 10:07:20');
+INSERT INTO `app_menu` VALUES (3, 'AI智能', 1, '2025-03-05 10:07:47', '2025-03-05 10:07:48');
 
 -- ----------------------------
 -- Table structure for app_node
@@ -46,20 +46,22 @@ CREATE TABLE `app_node`  (
   `menu_id` int(11) NULL DEFAULT NULL COMMENT '父菜单ID',
   `node_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '节点名称',
   `node_route` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '节点前端路由',
-  `node_rank` int(11) NULL DEFAULT NULL COMMENT '节点排名',
   `node_flag` tinyint(1) NULL DEFAULT NULL COMMENT '节点状态',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `app_node_id_uindex`(`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of app_node
 -- ----------------------------
-INSERT INTO `app_node` VALUES (1, 1, '测试路由一', '/demo/demo1', 1, 1, '2025-03-02 18:18:46', '2025-03-02 18:18:47');
-INSERT INTO `app_node` VALUES (2, 1, '测试路由二', '/demo/demo2', 2, 1, '2025-03-02 18:19:15', '2025-03-02 18:19:16');
-INSERT INTO `app_node` VALUES (3, 1, '测试路由三', '/demo/demo3', 3, 1, '2025-03-02 18:19:32', '2025-03-02 18:19:33');
+INSERT INTO `app_node` VALUES (1, 1, '个人记账', '/account/personage', 1, '2025-03-05 10:08:13', '2025-03-05 10:08:16');
+INSERT INTO `app_node` VALUES (2, 1, '共同记账', '/account/team', 1, '2025-03-05 10:08:34', '2025-03-05 10:08:35');
+INSERT INTO `app_node` VALUES (3, 1, '账单统计', '/account/statistic', 1, '2025-03-05 10:08:56', '2025-03-05 10:08:57');
+INSERT INTO `app_node` VALUES (4, 3, 'AI问答', '/agent/ask', 1, '2025-03-05 10:09:27', '2025-03-05 10:09:28');
+INSERT INTO `app_node` VALUES (5, 3, 'AI图像', '/agent/image', 1, '2025-03-05 10:09:45', '2025-03-05 10:09:46');
+INSERT INTO `app_node` VALUES (6, 3, 'AI视频', '/agent/video', 1, '2025-03-05 10:10:02', '2025-03-05 10:10:03');
 
 -- ----------------------------
 -- Table structure for operate_log
@@ -89,15 +91,14 @@ CREATE TABLE `role_app_auth`  (
   `menu_id` int(11) NULL DEFAULT NULL COMMENT '菜单ID',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `role_app_auth_id_uindex`(`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户角色与菜单权限对应表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户角色与菜单权限对应表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of role_app_auth
 -- ----------------------------
 INSERT INTO `role_app_auth` VALUES (1, 1, 1);
-INSERT INTO `role_app_auth` VALUES (2, 1, 2);
 INSERT INTO `role_app_auth` VALUES (3, 1, 3);
-INSERT INTO `role_app_auth` VALUES (4, 2, 1);
+INSERT INTO `role_app_auth` VALUES (4, 1, 1);
 
 -- ----------------------------
 -- Table structure for user
@@ -112,12 +113,13 @@ CREATE TABLE `user`  (
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '用户创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '用户修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (2, '程云博', '$2a$10$P/S2NXwl4QstLR5zHECfMebeEx44h/mtHw/2dxxGg2nHTUGGxfkmO', 1, 1, '2025-03-02 17:57:24', '2025-03-02 17:57:26');
+INSERT INTO `user` VALUES (5, '王蓉', '$2a$10$AWy/A3n2/KCRsBHt.c0iBOrW5Esg9uzlYReo99TzAzULsEuzBmjn.', 2, 1, '2025-03-05 10:42:50', '2025-03-05 10:42:50');
 
 -- ----------------------------
 -- Table structure for user_role
@@ -131,7 +133,7 @@ CREATE TABLE `user_role`  (
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '用户角色修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `user_role_id_uindex`(`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户角色' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户角色' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user_role
