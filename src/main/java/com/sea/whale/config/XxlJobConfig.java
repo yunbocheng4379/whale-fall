a60ecdbd41db783237607549c264702023e996cd
yunbocheng <2484720940@qq.com>
2025-03-10 22:23:35 +0800

    fix: :tada:升级并且优化xxl-job

--- src/main/java/com/sea/whale/config/XxlJobConfig.java
+++ src/main/java/com/sea/whale/config/XxlJobConfig.java
@@ -1,8 +1,7 @@
 //package com.sea.whale.config;
 //
 //import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+//import lombok.extern.slf4j.Slf4j;
 //import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
@@ -12,9 +11,9 @@
 // * @since 2023-04-24
 // */
 //@Configuration
+//@Slf4j
 //public class XxlJobConfig {
 //
-//    private Logger logger = LoggerFactory.getLogger(XxlJobConfig.class);
 //
 //    @Value("${xxl.job.admin.addresses}")
 //    private String adminAddresses;
@@ -22,19 +21,13 @@
 //    @Value("${xxl.job.accessToken}")
 //    private String accessToken;
 //
-//    @Value("${xxl.job.executor.appname}")
-//    private String appname;
-//
-//    @Value("${xxl.job.executor.address}")
-//    private String address;
-//
-//    @Value("${xxl.job.executor.ip}")
-//    private String ip;
+//    @Value("${xxl.job.executor.appName}")
+//    private String appName;
 //
 //    @Value("${xxl.job.executor.port}")
 //    private int port;
 //
-//    @Value("${xxl.job.executor.logpath}")
+//    @Value("${xxl.job.executor.logPath}")
 //    private String logPath;
 //
 //    @Value("${xxl.job.executor.logretentiondays}")
@@ -43,12 +36,10 @@
 //
 //    @Bean
 //    public XxlJobSpringExecutor xxlJobExecutor() {
-//        logger.info(">>>>>>>>>>> xxl-job config init.");
+//        log.info(">>>>>>>>>>> xxl-job config init.");
 //        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
 //        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
-//        xxlJobSpringExecutor.setAppname(appname);
-//        xxlJobSpringExecutor.setAddress(address);
-//        xxlJobSpringExecutor.setIp(ip);
+//        xxlJobSpringExecutor.setAppname(appName);
 //        xxlJobSpringExecutor.setPort(port);
 //        xxlJobSpringExecutor.setAccessToken(accessToken);
 //        xxlJobSpringExecutor.setLogPath(logPath);
